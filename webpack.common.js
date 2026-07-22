8502a32a94fdd857fc439f19444c962687b25c87
christian <cmchen678@gmail.com>
2026-07-22 16:41:25 -0400

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: "./src/template.html"
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
