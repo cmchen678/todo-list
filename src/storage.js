79eede1c25db319bbe0ead8c2b0c1fa39b5f523b
christian <cmchen678@gmail.com>
2026-08-05 14:52:30 -0400

    remove useless import

--- src/storage.js
+++ src/storage.js
@@ -1,5 +1,4 @@
 import { myProjects } from "./model.js";
-import { activeProject, activeTask } from "./index.js";
 
 export function populateStorage() {
     localStorage.setItem("myProjects", JSON.stringify(myProjects));
